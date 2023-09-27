26643d6be617c2eba893aa73159a04ff89683687
gbalachandhiran <inboxgowtham@yahoo.com>
2023-09-27 21:59:11 +0530

    Added linked list code 27th Sep 2023 v1

--- General Algorithms/SinglyLinkedList.py
+++ General Algorithms/SinglyLinkedList.py
@@ -105,3 +105,29 @@ llist.printList()
 print("\n")
 llist.removeNode("Thu")
 llist.printList()
+
+"""A linked list is a sequence of data elements, which are connected together via links. Each data element contains a connection to another data element in form of a pointer. Python does not have linked lists in its standard library. We implement the concept of linked lists using the concept of nodes as discussed in the previous chapter.
+
+We have already seen how we create a node class and how to traverse the elements of a node.In this chapter we are going to study the types of linked lists known as singly linked lists. In this type of data structure there is only one link between any two data elements. We create such a list and create additional methods to insert, update and remove elements from the list.
+
+Advantages Of Linked List:
+Dynamic data structure: A linked list is a dynamic arrangement so it can grow and shrink at runtime by allocating and deallocating memory. So there is no need to give the initial size of the linked list.
+No memory wastage: In the Linked list, efficient memory utilization can be achieved since the size of the linked list increase or decrease at run time so there is no memory wastage and there is no need to pre-allocate the memory.
+Implementation: Linear data structures like stacks and queues are often easily implemented using a linked list.
+Insertion and Deletion Operations: Insertion and deletion operations are quite easier in the linked list. There is no need to shift elements after the insertion or deletion of an element only the address present in the next pointer needs to be updated. 
+Flexible: This is because the elements in Linked List  are not stored in contiguous memory locations unlike the array.
+Efficient for large data: When working with large datasets linked lists play a crucial role as it can grow and shrink dynamically.
+Scalability: Contains the ability to add or remove elements at any position.
+Disadvantages Of Linked List:
+Memory usage: More memory is required in the linked list as compared to an array. Because in a linked list, a pointer is also required to store the address of the next element and it requires extra memory for itself.
+Traversal: In a Linked list traversal is more time-consuming as compared to an array. Direct access to an element is not possible in a linked list as in an array by index. For example, for accessing a node at position n, one has to traverse all the nodes before it.
+Reverse Traversing: In a singly linked list reverse traversing is not possible, but in the case of a doubly-linked list, it can be possible as it contains a pointer to the previously connected nodes with each node. For performing this extra memory is required for the back pointer hence, there is a wastage of memory.
+Random Access: Random access is not possible in a linked list due to its dynamic memory allocation.
+Lower efficiency at times: For certain operations, such as searching for an element or iterating through the list, can be slower in a linked list.
+Complex implementation:  The linked list implementation is more complex when compared to array. It requires a complex programming understanding.
+Difficult to share data: This is because it is not possible to directly access the memory address of an element in a linked list.
+Not suited for small dataset: Cannot provide any significant benefits on small dataset compare to that of an array.
+
+
+Reference : https://www.geeksforgeeks.org/advantages-and-disadvantages-of-linked-list/
+"""
